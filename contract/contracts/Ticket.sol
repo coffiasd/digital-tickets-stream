23a16c804ad68eddb34c73a7fc18a091304a8267
coffiasd <759291502@qq.com>
2023-06-04 11:15:32 +0800

    init

---
+++ contract/contracts/Ticket.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract Ticket is ERC721, Ownable {
+    using Counters for Counters.Counter;
+
+    //ticket _price.
+    uint256 private immutable _price;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor(uint256 price,string symbol,string name){
+        _name = name;
+        _symbol = symbol;
+        _price = price;
+    }
+
+    function publicMint() public {
+        require(msg.value>=_price,"price error");
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+
+    //onlyOwner
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+
+    //withDraw ether from contract.
+    function withDraw() public onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+}
--- pages/index.js
+++ pages/index.js
@@ -21,12 +21,16 @@ export default function Home() {
 
       <Alert />
 
-      <div className='min-h-screen'>
-        <div className='hero m-20'>
-          <Stats />
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">Hello there</h1>
+            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
         </div>
-
       </div>
+
       <Footer />
     </div >
   )
